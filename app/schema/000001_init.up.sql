b949d8c4b71df945afccc18e992e18d1fdd741f0
AntonZatsepilin <zatsepilin90.00@bk.ru>
2025-01-05 16:41:41 +0400

    Add migration tool and update Dockerfile; refine SQL schema

--- app/schema/000001_init.up.sql
+++ app/schema/000001_init.up.sql
@@ -2,12 +2,12 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,          
     email VARCHAR(255) NOT NULL UNIQUE,  
     username VARCHAR(50) UNIQUE,     
-    password_hash VARCHAR(255) NOT NULL,      
+    password_hash VARCHAR(255) NOT NULL   
 );
 
 CREATE TABLE refresh_tokens (
     id SERIAL PRIMARY KEY,          
     user_id INT REFERENCES users(id) ON DELETE CASCADE,
     token_hash VARCHAR(255) NOT NULL,  
-    expires_at TIMESTAMP NOT NULL,        
+    expires_at TIMESTAMP NOT NULL      
 );
